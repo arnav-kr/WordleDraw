24b9800bf3b2b6b84928be832138ac5ab9d48d43
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2025-08-14 10:26:26 +0530

    fix: header colors

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,4 @@
-<header class="flex items-center justify-center gap-3 p-4 px-6 border-1 border-b-zinc-50 dark:border-b-zinc-800 border-transparent text-white">
+<header class="flex items-center justify-center gap-3 p-4 px-6 border-1 border-b-zinc-50 dark:border-b-zinc-800 border-transparent text-black dark:text-white">
   <img src="/images/favicon.svg" alt="Wordle Draw Logo" class="w-8 h-8" />
   <h1 class="font-bold font-nyt text-lg">Wordle Draw</h1>
 </header>
